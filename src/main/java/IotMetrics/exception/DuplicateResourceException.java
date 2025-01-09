b90d36d4d3bc2fea57612a93d1c00591ff8bf0a5
Saracchandra23 <saracchandrac@gmail.com>
2025-01-08 21:52:09 -0700

    Add files via upload

---
+++ src/main/java/IotMetrics/exception/DuplicateResourceException.java
@@ -0,0 +1,8 @@
+package Saracchandra.Trucker.exception;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.web.bind.annotation.ResponseStatus;
+public class DuplicateResourceException extends RuntimeException {
+	public DuplicateResourceException(String msg){
+        super(msg);
+    }
+}
